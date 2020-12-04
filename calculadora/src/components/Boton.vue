902d133b20cf9aa2277296ce90e6ace16d12e7e2
Brondani00 <Brondani00@hotmail.com>
2020-12-04 00:04:51 -0300

    primer commit

---
+++ calculadora/src/components/Boton.vue
@@ -0,0 +1,27 @@
+
+<template>
+  <div>
+      
+  </div>
+  
+</template>
+
+<script>
+Vue.component('Boton', {
+    props: {value:String ,spanX:Number, spanY:Number}, 
+    data: function () 
+    { 
+    return {value}
+    },
+    template: '<Button  id="boton-id"  v-on:click="this.value">=</Button>',
+    
+})
+    value = document.getElementById("boton-id").value ;
+export default {
+    name: 'Boton',
+  components: { Boton }
+}
+</script>
+
+
+
